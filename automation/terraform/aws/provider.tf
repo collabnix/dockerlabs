d0d984cc66ca830c39f7986c411e8864bb4dab99
Ajeet Singh Raina, Docker Captain, {Code} Catalysts, Dell EMC R&D <ajeetraina@gmail.com>
2018-07-15 08:02:22 +0530

    Update provider.tf

--- automation/terraform/aws/provider.tf
+++ automation/terraform/aws/provider.tf
@@ -1,5 +1,6 @@
+# Setup our aws provider
 provider "aws" {
-  access_key = "XXXX"
-  secret_key = "XXXX"
-  region     = "us-east-1"
+  access_key  = "${var.aws_access_key_id}"
+  secret_key  = "${var.aws_secret_access_key}"
+  region      = "${var.region}"
 }
