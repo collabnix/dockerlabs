58c56463f4c1eae52514e37692d455e61cd73fd6
Ajeet Singh Raina, Docker Captain, {Code} Catalysts, Dell EMC R&D <ajeetraina@gmail.com>
2018-07-15 08:03:10 +0530

    Update and rename vars.tf to variables.tf

--- automation/terraform/aws/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {}
-variable "aws_access_key_id" {}
-variable "aws_secret_access_key" {}
-variable "aws_zones" {
- type        = "list"
- description = "List of availability zones to use"
- default     = ["us-east-1c", "us-east-1d", "us-east-1e"]
-}
