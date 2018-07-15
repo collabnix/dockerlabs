0552b7daad75ba2f9ee5730139bbb79e6ce7f1bf
Ajeet Singh Raina, Docker Captain, {Code} Catalysts, Dell EMC R&D <ajeetraina@gmail.com>
2018-07-15 08:05:09 +0530

    Delete main.tf

--- automation/terraform/aws/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  access_key = "XXXX"
-  secret_key = "XXXX"
-  region     = "us-east-1"
-}
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
-}
