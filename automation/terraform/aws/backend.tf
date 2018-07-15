0d358ad63b81f5ca8433c2025b9c87afda967d9e
Ajeet Singh Raina, Docker Captain, {Code} Catalysts, Dell EMC R&D <ajeetraina@gmail.com>
2018-07-15 08:04:58 +0530

    Delete backend.tf

--- automation/terraform/aws/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
- backend "s3" {
-   bucket         = "myorg-terraform-environmentname"
-   key            = "shared/terraform.tfstate"
-   region         = "us-east-1"
-   encrypt        = true
-   dynamodb_table = "terraform-lock"
- }
-}
