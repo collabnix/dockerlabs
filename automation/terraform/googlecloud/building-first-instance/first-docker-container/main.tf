8582b65befe798fa9ba7a2e75234dcffc28d26c2
Ajeet <ajeetraina@gmail.com>
2018-07-20 19:25:53 +0000

    Added Docker command

--- automation/terraform/googlecloud/building-first-instance/first-docker-container/main.tf
+++ automation/terraform/googlecloud/building-first-instance/first-docker-container/main.tf
@@ -43,7 +43,9 @@ resource "google_compute_instance" "www" {
     }
 
     inline = [
-      "sudo curl -sSL https://get.docker.com/ | sh"
+      "sudo curl -sSL https://get.docker.com/ | sh",
+      "sudo usermod -aG docker `echo $USER`", 
+      "sudo docker run -d -p 80:80 nginx"
     ]
   }
 
