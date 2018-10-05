4114411b3154aff6220af82b75bcfc48efe5f442
Ajeet Singh Raina, Docker Captain, {Code} Catalysts, Dell EMC R&D <ajeetraina@gmail.com>
2018-10-05 22:16:59 +0530

    Create install-ee.sh

---
+++ docker-ee/ubuntu/install-ee.sh
@@ -0,0 +1,17 @@
+sudo apt-get remove docker docker-engine docker-ce docker.io
+sudo apt update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+DOCKER_EE_URL=https://storebits.docker.com/ee/ubuntu/<>   ## ADD URL from store.docker.com
+DOCKER_EE_VERSION=stable-17.06
+curl -fsSL "${DOCKER_EE_URL}/ubuntu/gpg" | sudo apt-key add -
+sudo apt-key fingerprint 6D085F96
+sudo add-apt-repository \
+  "deb [arch=amd64] $DOCKER_EE_URL/ubuntu \
+  $(lsb_release -cs) \
+  $DOCKER_EE_VERSION"
+apt update
+apt install docker-ee
