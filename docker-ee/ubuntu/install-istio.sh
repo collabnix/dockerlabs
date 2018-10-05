114db51d83893d4cd2f6ba27ad95568787d6c72d
Ajeet Singh Raina, Docker Captain, {Code} Catalysts, Dell EMC R&D <ajeetraina@gmail.com>
2018-10-05 23:15:34 +0530

    Delete install-istio.sh

--- docker-ee/ubuntu/install-istio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -L https://git.io/getLatestIstio | sh -
-export PATH=$PWD/bin:$PATH
-cd istio-1.0.0
-kubectl apply -f install/kubernetes/helm/istio/templates/crds.yaml
-kubectl apply -f install/kubernetes/istio-demo.yaml
-kubectl get svc -n istio-system
-kubectl get pods -n istio-system
