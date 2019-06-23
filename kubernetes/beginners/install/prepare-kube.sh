b43c7ab0fa42715f09ff66706add7b7a8c84f0e2
Collabnix <34368930+collabnix@users.noreply.github.com>
2019-06-23 11:58:34 +0530

    Update and rename install-kube.sh to prepare-kube.sh

--- kubernetes/beginners/install/prepare-kube.sh
+++ kubernetes/beginners/install/prepare-kube.sh
@@ -6,10 +6,6 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - 
 cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list 
 deb https://apt.kubernetes.io/ kubernetes-xenial main 
-EOF 
-# Install Docker, Kubeadm, Kubelet, and Kubectl 
-sudo apt-get update 
-sudo apt-get install -y docker-ce kubelet kubeadm kubectl 
-# Enable net.bridge.bridge-nf-call-iptables
-echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf 
-sudo sysctl -p 
+>>EOF
+
+
