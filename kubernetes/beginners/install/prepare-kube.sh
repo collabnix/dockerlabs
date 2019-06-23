9115538c3f2ea0a67e3027d6c439119b33506def
Collabnix <34368930+collabnix@users.noreply.github.com>
2019-06-23 12:35:52 +0530

    Update prepare-kube.sh

--- kubernetes/beginners/install/prepare-kube.sh
+++ kubernetes/beginners/install/prepare-kube.sh
@@ -6,6 +6,5 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - 
 cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list 
 deb https://apt.kubernetes.io/ kubernetes-xenial main 
->>EOF
-
+EOF
 
